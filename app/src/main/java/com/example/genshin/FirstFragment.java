3a8de69a1c8e67ee56a38b0e70f248d27cb6c537
alvrivviv <alvaroriveravivero@gmail.com>
2024-11-25 12:12:00 +0100

    SI VAAAAAAAAAAAAAAAAAAAAAA (Hay que cambiar lo del gipi gipi)

--- app/src/main/java/com/example/genshin/FirstFragment.java
+++ app/src/main/java/com/example/genshin/FirstFragment.java
@@ -10,9 +10,13 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.room.Room;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,13 +26,13 @@ import retrofit2.Call;
 import com.example.genshin.databinding.FragmentFirstBinding;
 
 
-
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private ArrayList<Personaje> pjs = new ArrayList<>();
     private ArrayAdapter<Personaje> adapter;
     private boolean generar = true;
+
 
     @Override
     public View onCreateView(
@@ -38,7 +42,7 @@ public class FirstFragment extends Fragment {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         pjs = new ArrayList<>();
-        adapter = new adaptador(getContext(), R.layout.fragment_listapag1_personajes_genshin,pjs);
+        adapter = new adaptador(getContext(), R.layout.fragment_listapag1_personajes_genshin, pjs);
         binding.listaPersonajes.setAdapter(adapter);
         return binding.getRoot();
 
@@ -46,10 +50,19 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        generar = true;
-        int id = 1;     // empezará desde la id 1
-        int ultimaid = 88;      // y terminará al generar la id 88
-        llamarPjs(id, ultimaid);
+        AppDatabase db = AppDatabase.getDatabase(getContext());
+
+        db.getPersonajeDao().getPersonajes().observe(getViewLifecycleOwner(), new Observer<List<Personaje>>() {
+            @Override
+            public void onChanged(List<Personaje> personajes) {
+                // Cuando los datos cambian (es decir, cuando se obtienen los personajes)
+                // Actualizamos la lista de personajes
+                pjs.clear();  // Limpiamos la lista para evitar duplicados
+                pjs.addAll(personajes);  // Añadimos todos los personajes obtenidos
+                adapter.notifyDataSetChanged();  // Notificamos al adaptador que la lista ha cambiado
+            }
+        });
+
         binding.listaPersonajes.setOnItemClickListener((adapter, fragment, i, l) -> {
             Personaje pj = (Personaje) adapter.getItemAtPosition(i);
             Bundle args = new Bundle();
@@ -60,24 +73,28 @@ public class FirstFragment extends Fragment {
                     .navigate(R.id.action_FirstFragment_to_SecondFragment, args);
         });
     }
+
     private void llamarPjs(int id, int uid) {
-        if (id > uid){
-            Toast.makeText(getContext(),"Generado", Toast.LENGTH_SHORT).show();
+        if (id > uid) {
+            Toast.makeText(getContext(), "Generado", Toast.LENGTH_SHORT).show();
         } else {
             MetodosPjs metodospjs = new MetodosPjs();
 
             ExecutorService executor = Executors.newSingleThreadExecutor();
             executor.execute(() -> {
-                metodospjs.getPersonaje((id), Personaje -> {    // Usamos el metodo para obtener a los personajes por su id
-                    if (Personaje != null) {    //  Si el personaje que agarra no es null entonces genera el personaje
-                        getActivity().runOnUiThread(() -> {
-                            pjs.add(Personaje);     // Añadimos a la lista de personajes el personaje por la id que agarre
-                            PersonajeDAO cositas;
-                            //cositas.anadirpjs(pjs);
-                            adapter.notifyDataSetChanged(); // Notificamos al adaptador de que la array se ha modificado
-                            if (generar){
-                                llamarPjs(id + 1, uid);     //  Usamos recursividad
-                            }
+                metodospjs.getPersonaje((id), personaje -> {    // Usamos el metodo para obtener a los personajes por su id
+                    if (personaje != null) { //  Si el personaje que agarra no es null entonces genera el personaje
+                        ExecutorService executordb = Executors.newSingleThreadExecutor();
+                        executordb.execute(() -> {
+                            AppDatabase db = AppDatabase.getDatabase(getContext());
+                            db.getPersonajeDao().anadirpj(personaje);
+                            getActivity().runOnUiThread(() -> {
+                                pjs.add(personaje);     // Añadimos a la lista de personajes el personaje por la id que agarre
+                                adapter.notifyDataSetChanged(); // Notificamos al adaptador de que la array se ha modificado
+                                if (generar) {
+                                    llamarPjs(id + 1, uid);     //  Usamos recursividad
+                                }
+                            });
                         });
                     } else {    // Si no saltará un error y te notificará la id que ha fallado
                         getActivity().runOnUiThread(() ->
